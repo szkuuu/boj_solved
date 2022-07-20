9d2c89796478aec7313eb5268aecdf6bc57c34d9
souzoudev <robin_flux0d@icloud.com>
2022-07-20 14:06:55 +0900

    solved 8393

--- app/src/main/java/souzoudev/bojsolved/App.java
+++ app/src/main/java/souzoudev/bojsolved/App.java
@@ -3,10 +3,10 @@
  */
 package souzoudev.bojsolved;
 
-import souzoudev.bojsolved.problem.P10950;
+import souzoudev.bojsolved.problem.P8393;
 
 public class App {
     public static void main(String[] args) {
-        P10950.solution(args);
+        P8393.solution(args);
     }
 }
