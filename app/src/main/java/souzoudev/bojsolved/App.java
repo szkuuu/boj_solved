f8d5bfade7b06a9ea6cdb57db0058eb5ebb04e61
souzoudev <robin_flux0d@icloud.com>
2022-07-20 11:12:49 +0900

    solved 1000

--- app/src/main/java/souzoudev/bojsolved/App.java
+++ app/src/main/java/souzoudev/bojsolved/App.java
@@ -3,10 +3,10 @@
  */
 package souzoudev.bojsolved;
 
-import souzoudev.bojsolved.problem.P10172;
+import souzoudev.bojsolved.problem.P1000;
 
 public class App {
     public static void main(String[] args) {
-        P10172.solution(args);
+        P1000.solution(args);
     }
 }
