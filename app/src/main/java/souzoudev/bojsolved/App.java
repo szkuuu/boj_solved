c291b6ab3eccb1121dde5f5992addbc51921123f
souzoudev <robin_flux0d@icloud.com>
2022-07-20 11:21:19 +0900

    solved 18108

--- app/src/main/java/souzoudev/bojsolved/App.java
+++ app/src/main/java/souzoudev/bojsolved/App.java
@@ -3,10 +3,10 @@
  */
 package souzoudev.bojsolved;
 
-import souzoudev.bojsolved.problem.P10926;
+import souzoudev.bojsolved.problem.P18108;
 
 public class App {
     public static void main(String[] args) {
-        P10926.solution(args);
+        P18108.solution(args);
     }
 }
