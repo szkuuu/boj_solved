0f9ea57b3d6974acddd853172ca500262017bbd9
souzoudev <robin_flux0d@icloud.com>
2022-07-21 11:14:36 +0900

    solved 15596

---
+++ app/src/main/java/souzoudev/bojsolved/problem/P15596.java
@@ -0,0 +1,13 @@
+package souzoudev.bojsolved.problem;
+
+import java.util.Arrays;
+
+public class P15596 {
+    public static void solution(String[] args) {
+        
+    }
+
+    public long sum(int[] a) {
+        return Arrays.stream(a).asLongStream().sum();
+    }
+}
