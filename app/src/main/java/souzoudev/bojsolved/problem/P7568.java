92e95424c9b4ed0d69df41d476f6f9897b89ed1b
souzoudev <robin_flux0d@icloud.com>
2022-07-27 08:20:32 +0900

    solved 7568

--- app/src/main/java/souzoudev/bojsolved/problem/P7568.java
+++ app/src/main/java/souzoudev/bojsolved/problem/P7568.java
@@ -11,11 +11,6 @@ public class P7568 {
             int n = Integer.parseInt(br.readLine());
             int temp = n;
             int[][] list = new int[n][2];
-            int[] rank = new int[n];
-            int[] ret = new int[n];
-
-            Arrays.fill(rank, 0);
-            Arrays.fill(ret, 0);
 
             while (temp > 0) {
                 var tk = new StringTokenizer(br.readLine());
@@ -28,33 +23,21 @@ public class P7568 {
 
                 temp--;
             }
-
-            for (int idx = 0; idx < n; ++idx) {
-                for (int cidx = idx + 1; cidx < n; ++cidx) {
-                    if (list[idx][0] > list[cidx][0] && list[idx][1] > list[cidx][1]) {
-                        rank[idx] += 1;
-                        rank[cidx] -= 1;
-                    }
-
-                    if (list[idx][0] < list[cidx][0] && list[idx][1] < list[cidx][1]) {
-                        rank[cidx] += 1;
-                        rank[idx] -= 1;
-                    }
-                }
-            }
-
-            for (int idx = 0; idx < n; ++idx) {
-                int rankRet = 1;
-                for (int ridx = 0; ridx < n; ++ridx) {
-                    if (idx == ridx) continue;
-                    if (rank[idx] < rank[ridx]) ++rankRet;
-                }
-                ret[idx] = rankRet;
-            }
-
+            
             StringBuilder sb = new StringBuilder();
-
-            for (var i : ret) sb.append(i).append(' ');
+            
+            for (int i1 = 0; i1 < n; ++i1) {
+                int rankRet = 1;
+                for (int i2 = 0; i2 < n; ++i2) {
+                    if (i1 == i2) continue;
+                    
+                    if (list[i1][0] < list[i2][0] && list[i1][1] < list[i2][1]) {
+                        rankRet++;
+                    }
+                }
+                
+                sb.append(rankRet).append(' ');
+            }
             
             bw.write(sb.toString().trim());
         } catch (IOException e) {}
