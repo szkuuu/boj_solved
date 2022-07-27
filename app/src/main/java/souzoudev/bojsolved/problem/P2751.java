ff7f15e2d63e71de9b17eba40ba0edbf1b782dd3
souzoudev <robin_flux0d@icloud.com>
2022-07-27 10:35:53 +0900

    solved 2751

---
+++ app/src/main/java/souzoudev/bojsolved/problem/P2751.java
@@ -0,0 +1,26 @@
+package souzoudev.bojsolved.problem;
+
+import java.io.*;
+import java.util.*;
+
+public class P2751 {
+    public static void solution(String[] args) {
+        try (var br = new BufferedReader(new InputStreamReader(System.in));
+        var bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            int n = Integer.parseInt(br.readLine());
+            int[] a = new int[n];
+
+            for (int i = 0; i < a.length; ++i) {
+                a[i] = Integer.parseInt(br.readLine());
+            }
+
+            Arrays.sort(a);
+
+            for (var item : a) {
+                bw.write(Integer.toString(item));
+                bw.newLine();
+            }
+        } catch (IOException e) {}
+    }
+}
