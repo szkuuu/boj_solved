ca567b83748da7237a1e07d3e095802fdf819ec6
souzoudev <robin_flux0d@icloud.com>
2022-07-29 16:26:31 +0900

    solved 5086

--- app/src/main/java/souzoudev/bojsolved/App.java
+++ app/src/main/java/souzoudev/bojsolved/App.java
@@ -3,10 +3,10 @@
  */
 package souzoudev.bojsolved;
 
-import souzoudev.bojsolved.problem.P1358;
+import souzoudev.bojsolved.problem.P5086;
 
 public class App {
     public static void main(String[] args) {
-        P1358.solution(args);
+        P5086.solution(args);
     }
 }
