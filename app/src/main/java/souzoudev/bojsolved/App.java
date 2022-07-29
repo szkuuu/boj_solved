12d903ae65ec65be0055558301951d7f66783307
souzoudev <robin_flux0d@icloud.com>
2022-07-29 14:24:32 +0900

    solved 3053

--- app/src/main/java/souzoudev/bojsolved/App.java
+++ app/src/main/java/souzoudev/bojsolved/App.java
@@ -3,10 +3,10 @@
  */
 package souzoudev.bojsolved;
 
-import souzoudev.bojsolved.problem.P2477;
+import souzoudev.bojsolved.problem.P3053;
 
 public class App {
     public static void main(String[] args) {
-        P2477.solution(args);
+        P3053.solution(args);
     }
 }
